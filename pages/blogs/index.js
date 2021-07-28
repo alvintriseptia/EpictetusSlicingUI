ade91363ec1f6e65677f82a41d0a4064e57abaf4
Alvin Triseptia <alvintryseptia@gmail.com|>
2021-07-28 12:19:50 +0700

    third

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,4 +1,4 @@
-import { Footer, Navbar } from "../components";
+import { Footer, Navbar } from "../components/export";
 import InfoPost from "../components/Post/InfoPost";
 
 export default function blog() {
